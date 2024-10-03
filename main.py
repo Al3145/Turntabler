99dbb2b9cc5487686c5ef3fe67ddbb713333ef38
Kafxa <kafxaaa@gmail.com>
2024-10-03 04:48:14 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+bl_info = {
+    "name": "Preview Render Automation",
+    "author": "Al Ansari",
+    "version": (0, 0, 1),
+    "blender": (4.1),
+    "location": "View3D > Sidebar > Preview Render",
+    "description": "Automate preview renders with customizable settings.",
+    "category": "Render",
+}
